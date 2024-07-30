ad32a7aaf0caa05f0d3f219db3e9263dca39ade0
Joey Ahumada <JAhumad1@central.tucsonaz.gov>
2024-07-29 22:10:24 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import StreamList from './components/StreamList';
+import Movies from './components/Movies';
+import Cart from './components/Cart';
+import About from './components/About';
+import './App.css';
+
+function App () {
+    return (
+    <Router>
+      <div className="app-container">
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/movies">Movies</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <main className="content">
+          <Routes>
+            <Route path="/home" element={<StreamList />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
